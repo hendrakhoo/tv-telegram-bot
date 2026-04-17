2d7b99fd5ccbf449468f5ee9abe7716c26c869ec
hendrakhoo <57424320+hendrakhoo@users.noreply.github.com>
2026-04-17 09:57:51 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,35 @@
+from flask import Flask, request, jsonify
+import requests
+import os
+
+app = Flask(__name__)
+
+BOT_TOKEN = os.environ.get("8632618967:AAGQHWiTvjd2WFeeohznm4acZTojNi1lYrM")
+CHAT_ID = os.environ.get("-1003331541690")
+
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    data = request.json
+
+    symbol = data.get("symbol", "N/A")
+    price = data.get("price", "N/A")
+    signal = data.get("signal", "N/A")
+    timeframe = data.get("timeframe", "")
+
+    message = f"""
+🚀 {signal} {symbol}
+💰 Price: {price}
+⏰ TF: {timeframe}
+"""
+
+    url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+
+    requests.post(url, json={
+        "chat_id": CHAT_ID,
+        "text": message
+    })
+
+    return jsonify({"status": "ok"})
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
